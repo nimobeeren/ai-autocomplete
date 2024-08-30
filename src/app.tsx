9035d1b7db7bc119f7552f90c6bc6eff49ad2a81
Nimo Beeren <nimo.beeren@iodigital.com>
2024-08-30 14:43:31 +0200

    generate suggestions on type

--- src/app.tsx
+++ src/app.tsx
@@ -1,15 +1,49 @@
-import * as React from "react";
+import { useState } from "react";
 import { Search } from "./search";
 
 function App() {
-  const [value, setValue] = React.useState("");
-  const suggestions = ["Apple", "Banana", "Cherry", "Date", "Elderberry"];
+  const [value, setValue] = useState("");
+  const [suggestions, setSuggestions] = useState<string[]>([]);
+
+  async function generateSuggestions(value: string) {
+    if (!value) {
+      return [];
+    }
+
+    const session = await (window as any).ai.assistant.create({
+      initialPrompts: [
+        {
+          role: "system",
+          content: `Generate autocomplete suggestions for the input query.`,
+        },
+        { role: "user", content: "Generate autocomplete suggestions: apple" },
+        {
+          role: "assistant",
+          content: "* apple iphone\n* apple ipad\n* macbook pro",
+        },
+      ],
+    });
+
+    const result: string = await session.prompt(
+      `Generate autocomplete suggestions: ${value}`
+    );
+    console.log("raw result", result);
+    const newSuggestions = result
+      .split("\n")
+      .map((suggestion) => suggestion.replace("*", "").trim().toLowerCase());
+    console.log("suggestions", newSuggestions);
+    return newSuggestions;
+  }
 
   return (
     <main className="w-screen h-screen flex items-center justify-center">
       <Search
         value={value}
-        onChange={setValue}
+        onChange={async (newValue) => {
+          setValue(newValue);
+          const newSuggestions = await generateSuggestions(newValue);
+          setSuggestions(newSuggestions);
+        }}
         suggestions={suggestions}
       />
     </main>
