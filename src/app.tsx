a2c3a58e4117203e1787b0b4e096cb44a1552713
Nimo Beeren <nimo.beeren@iodigital.com>
2024-08-30 13:15:39 +0200

    render an input

---
+++ src/app.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className="w-screen h-screen flex items-center justify-center">
+      <input className="border-2 border-black rounded text-3xl px-2" />
+    </main>
+  );
+}
+
+export default App;
