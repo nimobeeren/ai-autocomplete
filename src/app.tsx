a6f5661c1de89f58613ec5a25512017c3d5bd258
Nimo Beeren <nimo.beeren@iodigital.com>
2024-09-16 10:35:24 +0200

    add debounce

--- src/app.tsx
+++ src/app.tsx
@@ -1,48 +1,64 @@
-import { useState } from "react";
+import { debounce } from "lodash-es";
+import { useEffect, useMemo, useState } from "react";
 import { Search } from "./search";
 
 function App() {
   const [value, setValue] = useState("");
   const [suggestions, setSuggestions] = useState<string[]>([]);
 
-  async function generateSuggestions(value: string) {
-    if (!value) {
-      return [];
-    }
+  const generateSuggestions = useMemo(() => {
+    const func = async (value: string) => {
+      if (!value) {
+        return setSuggestions([]);
+      }
 
-    const session = await window.ai.assistant.create({
-      initialPrompts: [
-        {
-          role: "system",
-          content: `Generate autocomplete suggestions for the input query.`,
-        },
-        { role: "user", content: "Generate autocomplete suggestions: apple" },
-        {
-          role: "assistant",
-          content: "* apple iphone\n* apple ipad\n* macbook pro",
-        },
-      ],
-    });
+      const session = await window.ai.assistant.create({
+        initialPrompts: [
+          {
+            role: "system",
+            content: "Generate autocomplete suggestions for the input query",
+          },
+          {
+            role: "user",
+            content: "Generate autocomplete suggestions: apple",
+          },
+          {
+            role: "assistant",
+            content: "* apple iphone\n* apple ipad\n* macbook pro",
+          },
+        ],
+      });
 
-    const result: string = await session.prompt(
-      `Generate autocomplete suggestions: ${value}`
-    );
-    console.log("raw result", result);
-    const newSuggestions = result
-      .split("\n")
-      .map((suggestion) => suggestion.replace("*", "").trim().toLowerCase());
-    console.log("suggestions", newSuggestions);
-    return newSuggestions;
-  }
+      console.log("start", value);
+      const result: string = await session.prompt(
+        `Generate autocomplete suggestions: ${value}`
+      );
+      // console.log("raw", result);
+      const newSuggestions = result
+        .split("\n")
+        .map((suggestion) => suggestion.replace("*", "").trim().toLowerCase());
+      // console.log("parsed", newSuggestions);
+      console.log("end", value);
+
+      setSuggestions(newSuggestions);
+    };
+    return debounce(func, 300);
+  }, []);
+
+  useEffect(() => {
+    console.log("canceling");
+    return () => {
+      generateSuggestions.cancel();
+    };
+  }, [generateSuggestions]);
 
   return (
     <main className="w-screen h-screen flex items-center justify-center">
       <Search
         value={value}
-        onChange={async (newValue) => {
+        onChange={(newValue) => {
           setValue(newValue);
-          const newSuggestions = await generateSuggestions(newValue);
-          setSuggestions(newSuggestions);
+          generateSuggestions(newValue);
         }}
         suggestions={suggestions}
       />
